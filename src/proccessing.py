b544a4ae844431cc8352770c50662fdd593ad1f9
oksun ian <yoksanochka@gmail.com>
2025-03-01 16:16:01 +1100

    lint black applied to correct the code

--- src/proccessing.py
+++ src/proccessing.py
@@ -1,17 +1,21 @@
 from typing import Any
 
-def filter_by_state(my_data:Any, state:str="EXECUTED") -> Any:
+
+def filter_by_state(my_data: Any, state: str = "EXECUTED") -> Any:
     """функция возвращает словарь по ключу state."""
     return [item for item in my_data if item.get("state") == state]
 
 
-def sort_by_date(data:Any, descending:bool=True) -> Any:
+def sort_by_date(data: Any, descending: bool = True) -> Any:
     """функция возвращает отсортированный по дате словарь."""
     return sorted(data, key=lambda x: x["date"], reverse=descending)
 
-data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-print(filter_by_state(data, state='CANCELED'))
+
+data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+print(filter_by_state(data, state="CANCELED"))
 print(sort_by_date(data, descending=False))
