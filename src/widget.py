e3dad6ea05087aa5a12da7c81b307a1c34917ddf
oksun ian <yoksanochka@gmail.com>
2025-03-02 10:03:50 +1100

    homework 10.1 corrected

--- src/widget.py
+++ src/widget.py
@@ -1,17 +1,16 @@
-import masks
-
+from src.masks import get_mask_account, get_mask_card_number
 
 def mask_card_account(my_info: str) -> str:
     """Функция, для вывода маски номера счета или номера карты"""
     if "Счет" in my_info:
-        return f"Счет {masks.get_mask_card_number(my_info)}"
+        return f"Счет {get_mask_account(my_info)}"
     else:
         new_info = my_info.split()
         if len(new_info) == 3:
-            masked_account_num = masks.get_mask_card_number(new_info[2])
+            masked_account_num = get_mask_card_number(new_info[2])
             return new_info[0] + " " + new_info[1] + " " + masked_account_num
         else:
-            masked_account_num = masks.get_mask_card_number(new_info[1])
+            masked_account_num = get_mask_card_number(new_info[1])
             return new_info[0] + " " + masked_account_num
 
 
@@ -20,9 +19,3 @@ def get_date(date_info: str) -> str:
     key_date = date_info.split("T")[0]
     year, month, day = key_date.split("-")
     return f"{day}.{month}.{year}"
-
-
-info_date = "2024-03-11T02:26:18.671407"
-input_info = "Visa Gold 5999414228426353"
-print(mask_card_account(input_info))
-print(get_date(info_date))
