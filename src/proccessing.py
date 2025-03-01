9466d1121c9974a20ab9dd8377d6e186e95487dd
oksun ian <yoksanochka@gmail.com>
2025-03-02 10:07:18 +1100

    homework 10.1 corrected

--- src/proccessing.py
+++ src/proccessing.py
@@ -11,13 +11,3 @@ def sort_by_date(my_data: List[Dict[str, Any]], descending: bool = True) -> List
     """Функция на входе принимает словарь аккаунтов (id) со статусами (state) и датой (date)
     и возвращает отсортированный по дате словарь."""
     return sorted(my_data, key=lambda x: x["date"], reverse=descending)
-
-
-data = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-print(filter_by_state(data, state="CANCELED"))
-print(sort_by_date(data, descending=False))
