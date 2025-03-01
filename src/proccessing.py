5a6d2f47d3787d9295818829f3c6589e687126f1
oksun ian <yoksanochka@gmail.com>
2025-03-01 15:51:12 +1100

    annotation added

--- src/proccessing.py
+++ src/proccessing.py
@@ -1,8 +1,10 @@
-def filter_by_state(my_id, state="EXECUTED"):
-    """ функция возвращает словарь по ключу state."""
-    return [item for item in my_id if item.get('state') == state]
+from typing import Any
+
+def filter_by_state(my_data:Any, state:str="EXECUTED") -> Any:
+    """функция возвращает словарь по ключу state."""
+    return [item for item in my_data if item.get("state") == state]
 
 
-def sort_by_date(data, descending=True):
+def sort_by_date(data:Any, descending:bool=True) -> Any:
     """функция возвращает отсортированный по дате словарь."""
-    return sorted(data, key=lambda x: x['date'], reverse=descending)
+    return sorted(data, key=lambda x: x["date"], reverse=descending)
