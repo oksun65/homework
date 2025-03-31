b08e559e98279a76c4e2a28d4f53a7f88a665513
oksun ian <yoksanochka@gmail.com>
2025-03-31 21:21:46 +1100

    docstrings added

--- src/generators.py
+++ src/generators.py
@@ -1,15 +1,18 @@
 def filter_by_currency(transactions, currency_name):
+    """данная функция принимает на вход список словарей и возвращает итерацию, отфильтрованную по валюте"""
     for tran in transactions:
         if tran.get("operationAmount", {}).get("currency", {}).get("code") == currency_name:
             yield tran
 
 
 def transaction_descriptions(transactions):
+    """данная функция принимает на вход список словарей и возвращает описание всех транзакций"""
     for tran in transactions:
         yield tran.get("description")
 
 
 def card_number_generator(start, finish):
+    """Данная функция генерирует номера карты, в заданном диапазоне"""
 
     for num in range(start, finish + 1):
         str_num = str(num)
